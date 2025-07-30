70a2c73608f7bee173d482e23ac6a73325bcc7c4
mazhanbaig <mazhanbaig44@gmail.com>
2025-07-30 20:00:30 +0500

    shape box task

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-let para=document.getElementById("para");
-para.addEventListener('click',()=>{
-  setInterval(() => {
-  para.classList.toggle("hidden");
-}, 1000);
+// let para=document.getElementById("para");
+// para.addEventListener('click',()=>{
+//   setInterval(() => {
+//   para.classList.toggle("hidden");
+// }, 1000);
+// })
 
 
-})
+
+let box = document.getElementById("box");
+let squarebtn = document.getElementById("square");
+let circlebtn = document.getElementById("circle");
+let recbtn = document.getElementById("rectangle");
+
+function setShape(shape) {
+    box.className = ""; 
+    box.classList.add(shape);
+}
+
+squarebtn.addEventListener("click", () => {
+    setShape("square");
+});
+
+circlebtn.addEventListener("click", () => {
+    setShape("circle");
+});
+
+recbtn.addEventListener("click", () => {
+    setShape("rectangle");
+});
